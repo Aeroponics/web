0d5310c7b2963cb39fad9ff784144d7e487c4e13
Soma Mizobuchi <somamizobuchi@somas-mbp.home>
2020-03-26 11:06:37 -0400

    minor tweaks

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import MainNav from './Components/MainNav'
+import About from './Components/About'
+import Home from './Components/Home'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+
 
 function App() {
+
+  useEffect(() => {
+    
+  });
+
   return (
     <div className="App">
+      <Router>
       <MainNav />
+        <Switch>
+          <Route path="/About"><About /></Route>
+          <Route path="/"><Home /></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
