f594a57046c0892b7658507d1f9f95e0e7306ec3
Soma Mizobuchi <somamizobuchi@somas-mbp.home>
2020-03-26 16:21:10 -0400

    updates to api server

--- models/Plant.js
+++ models/Plant.js
@@ -6,11 +6,16 @@ const logSchema = new Schema({
     temp: Number,
     humidity: Number,
     img: String
+},{
+    timestamps: true
 })
+
 const plantSchema = new Schema({
     name: String,
     scientific: String,
     logs: [logSchema]
+},{
+    timestamps: true
 })
 
-module.exports = Plant = mongoose.model('Plant', plantSchema)
+module.exports = Plant = mongoose.model('plant', plantSchema)
